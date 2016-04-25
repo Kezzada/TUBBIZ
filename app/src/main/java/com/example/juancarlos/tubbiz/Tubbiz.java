4802e29a0322ef1a9edd27253349ea865bc04aaf
RayMithos <pedroralvarez@gmail.com>
2016-04-25 08:18:11 +0200

    Cambios en la estructura, diseño de la primera pantalla y logo

--- app/src/main/java/com/example/juancarlos/tubbiz/Tubbiz.java
+++ app/src/main/java/com/example/juancarlos/tubbiz/Tubbiz.java
@@ -2,21 +2,31 @@ package com.example.juancarlos.tubbiz;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Tubbiz extends Activity {
+
+    protected TextView fuentePers;
+    protected String font_titulo = "font/Roboto-Regular.ttf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tubbiz);
+        fuentePers = (TextView) findViewById(R.id.registrar);
+        Typeface font = Typeface.createFromAsset(getAssets(), font_titulo);
+        fuentePers.setTypeface(font);
+
     }
 
     public void lanzarLogin(View view) {
         Intent i = new Intent(this, Login.class);
         startActivity(i);
     }
+
     public void lanzarRegistro(View view) {
         Intent i = new Intent(this, Registro.class);
         startActivity(i);
